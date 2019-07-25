d3738705f63bfd6d606f45c8b4e01465517a4534
April Mintac Pineda <aprilmintacpineda@gmail.com>
2019-07-25 15:13:46 +0800

    Updated tests for creating a board; added tests for editing a board;

--- user-activities/__tests__/selenium/board.spec.js
+++ user-activities/__tests__/selenium/board.spec.js
@@ -23,18 +23,26 @@ class CreateBoard {
   }
 
   async fillTitle () {
-    const title = `Created by Selenium. ${this.testId}#${this.count}`;
+    const title = `${this.testId}#${this.count}`;
     await this.driver.findElement(By.xpath('//*[@id="create-board"]/div[1]/input')).sendKeys(title);
-    this.count += 1;
+    this.count++;
     return title;
-  }
-
-  async fillDescription () {
-    await this.driver.findElement(By.xpath('//*[@id="create-board"]/div[2]/textarea')).sendKeys('This board was created by Selenium.');
   }
 
   async clearTitle () {
     await this.driver.findElement(By.xpath('//*[@id="create-board"]/div[1]/input')).clear();
+    await sleep(100);
+  }
+
+  async fillDescription () {
+    const description = `${this.testId}#${this.count}`;
+    await this.driver.findElement(By.xpath('//*[@id="create-board"]/div[2]/textarea')).sendKeys(description);
+    this.count++;
+    return description;
+  }
+
+  async clearDescription () {
+    await this.driver.findElement(By.xpath('//*[@id="create-board"]/div[2]/textarea')).clear();
     await sleep(100);
   }
 
@@ -48,12 +56,10 @@ module.exports = (driver, testId) => {
   const CreateBoardForm = new CreateBoard(driver, testId);
 
   return {
-    async beforeEach () {
-      await CreateBoardForm.open();
-    },
     tests: [
       async () => {
         // test validation message of title
+        await CreateBoardForm.open();
         await CreateBoardForm.fillTitle();
         await CreateBoardForm.clearTitle();
         logTestResult(
@@ -64,6 +70,7 @@ module.exports = (driver, testId) => {
       },
       async () => {
         // submitting an empty form
+        await CreateBoardForm.open();
         await CreateBoardForm.submit();
         logTestResult(
           'CreateBoard > Submit empty form',
@@ -73,6 +80,13 @@ module.exports = (driver, testId) => {
       },
       async () => {
         // can create board without description
+        await CreateBoardForm.open();
+        await CreateBoardForm.fillTitle();
+        await CreateBoardForm.clearTitle();
+        logTestResult(
+          'CreateBoard > Creating without description validates title',
+          await driver.findElement(By.xpath('//*[@id="create-board"]/div[1]/p')).getText() === 'Please enter the name of this board.'
+        );
         const title = await CreateBoardForm.fillTitle();
         await CreateBoardForm.submit();
         logTestResult(
@@ -82,12 +96,62 @@ module.exports = (driver, testId) => {
       },
       async () => {
         // can create board with description
+        await CreateBoardForm.open();
+        await CreateBoardForm.fillTitle();
+        await CreateBoardForm.clearTitle();
+        logTestResult(
+          'CreateBoard > Creating with description validates title',
+          await driver.findElement(By.xpath('//*[@id="create-board"]/div[1]/p')).getText() === 'Please enter the name of this board.'
+        );
         const title = await CreateBoardForm.fillTitle();
         await CreateBoardForm.fillDescription();
         await CreateBoardForm.submit();
         logTestResult(
           'CreateBoard > Creating with description',
           await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/p[1]/strong')).getText() === title
+        );
+      },
+      async () => {
+        // editing a board
+
+        // edit board form validates title
+        await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/div/button[1]')).click();
+        await CreateBoardForm.clearTitle();
+        logTestResult(
+          'CreateBoard > Editing a board title validates title',
+          await driver.findElement(By.xpath('//*[@id="create-board"]/div[1]/p')).getText() === 'Please enter the name of this board.'
+        );
+
+        // can cancel in the middle of editing
+        await CreateBoardForm.cancel();
+
+        // can change title
+        await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/div/button[1]')).click();
+        await CreateBoardForm.clearTitle();
+        const title = await CreateBoardForm.fillTitle();
+        await CreateBoardForm.submit();
+        logTestResult(
+          'CreateBoard > Editing a board allows changing title',
+          await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/p[1]/strong')).getText() === title
+        );
+
+        // can change description
+        await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/div/button[1]')).click();
+        await CreateBoardForm.clearDescription();
+        const description = await CreateBoardForm.fillDescription();
+        await CreateBoardForm.submit();
+        logTestResult(
+          'CreateBoard > Editing a board allows changing description',
+          await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/p[2]')).getText() === description
+        );
+
+        // can remove description
+        await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/div/button[1]')).click();
+        await CreateBoardForm.clearDescription();
+        await CreateBoardForm.submit();
+        logTestResult(
+          'CreateBoard > Editing a board allows removing the description',
+          await driver.findElement(By.xpath('//*[@id="boards"]/div[2]/p[2]')).getText() === 'No details provided.'
         );
       }
     ]
